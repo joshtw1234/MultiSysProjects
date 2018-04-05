db199e23137e0e31af9200c350a2e2fd986bfb10
Josh <josh.tseng@hp.com>
2018-04-06 01:28:14 +0800

    Add HID Demo project Add HID Library.

---
+++ HIDDemo/HIDDemoLib.xaml.cs
@@ -0,0 +1,65 @@
+﻿using HIDDemo.Views;
+using System;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using UtilityUILib;
+
+namespace HIDDemo
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class HIDDemoLib : UserControl, IModuleInterface
+    {
+        public HIDDemoLib()
+        {
+            InitializeComponent();
+        }
+
+        public string interfaceVersion => throw new NotImplementedException();
+
+        public string moduleName
+        {
+            get
+            {
+                return "HIDDemo";
+            }
+        }
+
+        public BitmapImage getModuleLogoImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BitmapImage getModuleLogoImage2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int hide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void initialize()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public bool isPlatformSupported()
+        {
+            //throw new NotImplementedException();
+            return true;
+        }
+
+        public int show()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void uninitialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

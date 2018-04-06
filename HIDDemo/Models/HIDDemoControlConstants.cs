40f0c7e798d7fa6d9eea7afb54b0c17354a14c4e
Josh <josh.tseng@hp.com>
2018-04-06 17:07:54 +0800

    Add objects

---
+++ HIDDemo/Models/HIDDemoControlConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIDDemo.Models
+{
+    public enum HIDDisplayItemEnum
+    {
+        Lebel,
+        TextBlock
+    }
+    public class HIDDemoControlConstants
+    {
+    }
+}
---
+++ HIDDemo/Models/HIDDisplayItem.cs
@@ -0,0 +1,20 @@
+﻿using UtilityUILib;
+
+namespace HIDDemo.Models
+{
+    public class HIDDisplayItem : MenuItem
+    {
+        private HIDDisplayItemEnum displayType;
+        public HIDDisplayItemEnum DisplayType
+        {
+            get
+            {
+                return displayType;
+            }
+            set
+            {
+                displayType = value;
+            }
+        }
+    }
+}

c70746c98c4cea1b94ca0e3ad9736711fb0b4513
Josh Tseng <josh.tseng@hp.com>
2018-06-20 17:13:45 +0800

    Add Entry file.

---
+++ HIDHeadSet/HIDHeadSetLib.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using UtilityUILib;
+
+namespace HIDHeadSet
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class HIDHeadSet : UserControl, IModuleInterface
+    {
+        public HIDHeadSet()
+        {
+            InitializeComponent();
+        }
+
+        public string interfaceVersion => throw new System.NotImplementedException();
+
+        public string moduleName
+        {
+            get
+            {
+                return "HIDHeadSet";
+            }
+        }
+
+        public BitmapImage getModuleLogoImage()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BitmapImage getModuleLogoImage2()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int hide()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void initialize()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public bool isPlatformSupported()
+        {
+            //throw new System.NotImplementedException();
+            return true;
+        }
+
+        public int show()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void uninitialize()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

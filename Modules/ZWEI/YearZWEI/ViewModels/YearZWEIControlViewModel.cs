55d0a4aab4c98850c8be391c2df93305e5cf3185
Josh <josh.tseng@hp.com>
2018-07-22 15:23:46 +0800

    Add MVVM

---
+++ Modules/ZWEI/YearZWEI/ViewModels/YearZWEIControlViewModel.cs
@@ -0,0 +1,13 @@
+﻿using YearZWEI.Models;
+
+namespace YearZWEI.ViewModels
+{
+    class YearZWEIControlViewModel
+    {
+        IYearZWEIModel zWeiModel;
+        public YearZWEIControlViewModel(IYearZWEIModel yzWeiModel)
+        {
+            zWeiModel = yzWeiModel;
+        }
+    }
+}

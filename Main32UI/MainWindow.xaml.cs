80b9bc2224cb5906a4d02ce78e17dc35921b7677
Josh Tseng <josh.tseng@hp.com>
2018-07-25 12:14:09 +0800

    Clean code.

--- Main32UI/MainWindow.xaml.cs
+++ Main32UI/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using UtilityUILib;
 
 namespace Main32UI

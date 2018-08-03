cad98a257b13e40c4c9c066ffe67c0d66290bd72
Josh Tseng <josh.tseng@hp.com>
2018-08-03 09:41:44 +0800

    update file.

---
+++ Modules/MVVMDemo/WPFMVVM/ViewModels/WPFMVVMControlViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using UtilityUILib;
+using WPFMVVM.Models;
+
+namespace WPFMVVM.ViewModels
+{
+    public class WPFMVVMControlViewModel : BindAbleBases
+    {
+        public WPFMVVMControlViewModel(IWPFMVVMModel _model)
+        {
+
+        }
+
+    }
+}

d9419c8f27c5c19abb0927958809071b2cd003b5
Josh Tseng <josh.tseng@hp.com>
2018-09-06 15:21:05 +0800

    Add ControlTemplate DataTemplate Style xaml file.

--- Modules/MVVMDemo/WPFMVVM/ViewModels/WPFMVVMControlViewModel.cs
+++ Modules/MVVMDemo/WPFMVVM/ViewModels/WPFMVVMControlViewModel.cs
@@ -5,10 +5,30 @@ namespace WPFMVVM.ViewModels
 {
     public class WPFMVVMControlViewModel : BindAbleBases
     {
+        SliderDataContext _sliderCore;
+        public SliderDataContext SliderCore
+        {
+            get
+            {
+                return _sliderCore;
+            }
+            set
+            {
+                _sliderCore = value;
+                onPropertyChanged(this, "SliderCore");
+            }
+        }
         public WPFMVVMControlViewModel(IWPFMVVMModel _model)
         {
-
+            _sliderCore = new SliderDataContext() { CoreMax = 100, CoreMin = 50, CoreTick = 10 };
         }
 
+    }
+
+    public class SliderDataContext
+    {
+        public double CoreMax { get; set; }
+        public double CoreMin { get; set; }
+        public double CoreTick { get; set; }
     }
 }

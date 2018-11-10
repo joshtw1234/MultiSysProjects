0d228e6b3be994729f5a8ca1fd66031665067201
Josh Tseng <josh.tseng@hp.com>
2018-11-11 00:10:05 +0800

    Add DirectShowDemo project.

---
+++ Modules/ProfileTest/DirectShowDemo/DirectShowDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DirectShowDemo.Models;
+
+namespace DirectShowDemo.ViewModels
+{
+    class MainWindowViewModel
+    {
+        IMainWindowModel _model;
+        public MainWindowViewModel(IMainWindowModel model)
+        {
+            _model = model;
+        }
+    }
+}

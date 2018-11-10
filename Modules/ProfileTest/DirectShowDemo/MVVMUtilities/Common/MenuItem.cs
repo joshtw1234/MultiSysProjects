6435b7ae00468d4bab32390b4941e87b3802ecf4
Josh Tseng <josh.tseng@hp.com>
2018-11-11 00:27:26 +0800

    Add Common MVVM

---
+++ Modules/ProfileTest/DirectShowDemo/MVVMUtilities/Common/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace MVVMUtilities.Common
+{
+    public class MenuItem : IMenuItem
+    {
+        public string MenuName { get; set; }
+        public string MenuImage { get; set; }
+        public string MenuImagePressed { get; set; }
+        public string MenuData { get; set; }
+        public Style MenuStyle { get; set; }
+        public bool MenuEnabled { get; set; }
+        public bool MenuChecked { get; set; }
+        public bool MenuVisibility { get; set; }
+        public MyDelegateCommond<string> MenuCommand { get; set; }
+    }
+
+    public class BindAbleBases : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Interface
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void onPropertyChanged(object sender, string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            { PropertyChanged(sender, new PropertyChangedEventArgs(propertyName)); }
+        }
+        #endregion
+    }
+
+}

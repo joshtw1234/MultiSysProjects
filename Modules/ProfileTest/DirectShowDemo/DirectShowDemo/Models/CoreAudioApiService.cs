b9f80388b08aa51d3d2896dfb1a1b289dd529137
Josh Tseng <josh.tseng@hp.com>
2018-11-12 02:38:59 +0800

    Add CoreAudioApiService.cs

---
+++ Modules/ProfileTest/DirectShowDemo/DirectShowDemo/Models/CoreAudioApiService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DirectShowDemo.Models
+{
+    class CoreAudioApiService
+    {
+        //[DllImport("CoreAudioApisDll.dll", EntryPoint = "nCoreAudioApisDll", CharSet = CharSet.Auto)]
+        //internal static extern int nCoreAudioApisDll;
+        [DllImport("CoreAudioApisDll.dll", EntryPoint = "fnCoreAudioApisDll", CharSet = CharSet.Auto)]
+        internal static extern int fnCoreAudioApisDll();
+    }
+}

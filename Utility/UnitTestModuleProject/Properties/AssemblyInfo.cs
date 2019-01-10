7140782d17fe0404ca4faff577d6bf8adbbaef13
Josh Tseng <josh.tseng@hp.com>
2019-01-10 15:21:14 +0800

    Add unittest back.

---
+++ Utility/UnitTestModuleProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestModuleProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestModuleProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b17d4dc-9d34-44d6-83c0-9a5ffc6cece2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Utility/UnitTestModuleProject/UnitTestHID.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestModuleProject
+{
+    [TestClass]
+    public class UnitTestHID
+    {
+        [TestMethod]
+        public void TestBrowseHID()
+        {
+            var result = HIDLib.HIDAPIs.BrowseHID();
+            Assert.IsNotNull(result);
+        }
+    }
+}

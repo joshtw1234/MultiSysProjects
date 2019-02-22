bb57ef3d552566956b4baa46e7c9c6b74ff6bdd9
Josh Tseng <josh.tseng@hp.com>
2019-02-22 18:30:11 +0800

    Fix nuget issue.

--- Modules/ProfileTest/PrismDemo/PrismDemo/Bootstrapper.cs
+++ Modules/ProfileTest/PrismDemo/PrismDemo/Bootstrapper.cs
@@ -9,26 +9,11 @@ namespace PrismDemo
     {
         #region Base Bootstrapper.
 #if true
-        protected override DependencyObject CreateShell()
-        {
-            return base.CreateShell();
-            //return Container.Resolve<MainWindow>();
-        }
-
+ 
         protected override void ConfigureContainer()
         {
             base.ConfigureContainer();
             this.RegisterTypeIfMissing(typeof(IMainWindowModel), typeof(MainWindowModel), true);
-        }
-
-        protected override void ConfigureModuleCatalog()
-        {
-            base.ConfigureModuleCatalog();
-        }
-
-        protected override void InitializeModules()
-        {
-            base.InitializeModules();
         }
 
 #endif

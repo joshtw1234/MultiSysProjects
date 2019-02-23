40fe26849f93b4fa77addc8ed2a4a47488726620
Josh <josh.tseng@hp.com>
2019-02-23 21:31:39 +0800

    Add CommonUILib.csproj

---
+++ Modules/ProfileTest/PrismDemo/Libraries/CommonUILib/Models/ViewItem.cs
@@ -0,0 +1,19 @@
+﻿using CommonUILib.Interfaces;
+using Prism.Commands;
+using System.Windows;
+
+namespace CommonUILib.Models
+{
+    class ViewItem : IViewItem
+    {
+        public string MenuName { get; set; }
+        public string MenuImage { get; set; }
+        public string MenuImagePressed { get; set; }
+        public string MenuData { get; set; }
+        public DelegateCommand<string> MenuCommand { get; set; }
+        public Style MenuStyle { get; set; }
+        public bool MenuEnabled { get; set; }
+        public bool MenuChecked { get; set; }
+        public bool MenuVisibility { get; set; }
+    }
+}

1d8bfda87f2e66634520c133355d959d10549a86
Josh Tseng <josh.tseng@hp.com>
2019-03-07 13:39:44 +0800

    Add Desktop bridge test file system.

---
+++ Modules/ProfileTest/DesktopBridgeTest/DesktopBridgeTest/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DesktopBridgeTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //openFileDialog = new OpenFileDialog()
+            //{
+            //    FileName = "Select a text file",
+            //    Filter = "Text files (*.txt)|*.txt",
+            //    Title = "Open text file"
+            //};
+        }
+        private OpenFileDialog openFileDialog;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Name.Equals("save"))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                if (saveFileDialog.ShowDialog() == true)
+                    File.WriteAllText(saveFileDialog.FileName, "This is Josh");
+
+            }
+            else
+            {
+                openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    try
+                    {
+                        Path.Text = openFileDialog.FileName;
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}

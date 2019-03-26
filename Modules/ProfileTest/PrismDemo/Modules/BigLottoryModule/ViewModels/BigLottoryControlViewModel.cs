df0c9ba52e08312ea9d79a094b53dfb07c4601ad
Josh <josh.tseng@hp.com>
2019-03-26 21:56:45 +0800

    Add DownloadWeb

--- Modules/ProfileTest/PrismDemo/Modules/BigLottoryModule/ViewModels/BigLottoryControlViewModel.cs
+++ Modules/ProfileTest/PrismDemo/Modules/BigLottoryModule/ViewModels/BigLottoryControlViewModel.cs
@@ -1,4 +1,7 @@
-﻿using BigLottoryModule.Interface;
+﻿using System;
+using System.IO;
+using System.Net;
+using BigLottoryModule.Interface;
 
 namespace BigLottoryModule.ViewModels
 {
@@ -8,6 +11,27 @@ namespace BigLottoryModule.ViewModels
         public BigLottoryControlViewModel(IBigLottoryControlModel model)
         {
             _model = model;
+            string webLink = "https://www.pilio.idv.tw/ltobig/ServerC/list.asp?indexpage=1&orderby=new";
+            string currentPath = Directory.GetCurrentDirectory();
+            string lottoryDir = $"{currentPath}\\LottoryData";
+            string saveWebFile = "Lottory";
+            if (!Directory.Exists(lottoryDir))
+            {
+                Directory.CreateDirectory(lottoryDir);
+            }
+            for (int i = 1; i < 72; i++)
+            {
+                webLink = $"https://www.pilio.idv.tw/ltobig/ServerC/list.asp?indexpage={i}&orderby=new";
+                saveWebFile = $"Lottory{i}.txt";
+                DownloadWeb(webLink, lottoryDir, saveWebFile);
+            }
+            
+        }
+
+        private void DownloadWeb(string webLink, string saveDir, string saveFile)
+        {
+            WebClient client = new WebClient();
+            client.DownloadFile(webLink, Path.Combine(saveDir, saveFile));
         }
     }
 }

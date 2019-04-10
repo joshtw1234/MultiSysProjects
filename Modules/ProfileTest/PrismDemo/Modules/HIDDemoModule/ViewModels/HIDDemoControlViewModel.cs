b09c088435cd53de4aa9541f98cbd781dcfec483
Josh Tseng <josh.tseng@hp.com>
2019-04-10 17:33:18 +0800

    Implement IConfirmNavigationRequest

--- Modules/ProfileTest/PrismDemo/Modules/HIDDemoModule/ViewModels/HIDDemoControlViewModel.cs
+++ Modules/ProfileTest/PrismDemo/Modules/HIDDemoModule/ViewModels/HIDDemoControlViewModel.cs
@@ -1,18 +1,44 @@
 ﻿using HIDDemoModule.Interfaces;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace HIDDemoModule.ViewModels
 {
-    class HIDDemoControlViewModel
+    class HIDDemoControlViewModel : IConfirmNavigationRequest
     {
         IHIDDemoControlModel _model;
         public HIDDemoControlViewModel(IHIDDemoControlModel model)
         {
             _model = model;
+        }
+
+        bool isPass = false;
+        public void ConfirmNavigationRequest(NavigationContext navigationContext, Action<bool> continuationCallback)
+        {
+            MessageBox.Show($"isPass {isPass}");
+            continuationCallback(isPass);
+            isPass = !isPass;
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            //throw new NotImplementedException();
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            //throw new NotImplementedException();
         }
     }
 }

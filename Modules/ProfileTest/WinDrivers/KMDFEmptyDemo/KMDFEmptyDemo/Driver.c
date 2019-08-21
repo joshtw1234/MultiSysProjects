4b49ef64451879c957ed1875fcc0082416afd486
Josh Tseng <josh.tseng@hp.com>
2019-08-21 14:20:28 +0800

    add empty KMDF with build able.

---
+++ Modules/ProfileTest/WinDrivers/KMDFEmptyDemo/KMDFEmptyDemo/Driver.c
@@ -0,0 +1,68 @@
+//Follow MSDN to create and build able
+//https://docs.microsoft.com/en-us/windows-hardware/drivers/gettingstarted/writing-a-very-small-kmdf--driver
+
+
+#include <ntddk.h>
+#include <wdf.h>
+
+//The Driver Entry
+DRIVER_INITIALIZE DriverEntry;
+//The Call back from System when Device Added
+EVT_WDF_DRIVER_DEVICE_ADD KmdfEmptyDemoEvtDeviceAdd;
+
+NTSTATUS
+DriverEntry(
+	_In_ PDRIVER_OBJECT     DriverObject,
+	_In_ PUNICODE_STRING    RegistryPath
+)
+{
+	// NTSTATUS variable to record success or failure
+	NTSTATUS status = STATUS_SUCCESS;
+
+	// Allocate the driver configuration object
+	WDF_DRIVER_CONFIG config;
+
+	// Print "Hello World" for DriverEntry
+	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
+
+	// Initialize the driver configuration object to register the
+	// entry point for the EvtDeviceAdd callback, KmdfHelloWorldEvtDeviceAdd
+	WDF_DRIVER_CONFIG_INIT(&config,
+		KmdfEmptyDemoEvtDeviceAdd
+	);
+
+	// Finally, create the driver object
+	status = WdfDriverCreate(DriverObject,
+		RegistryPath,
+		WDF_NO_OBJECT_ATTRIBUTES,
+		&config,
+		WDF_NO_HANDLE
+	);
+	return status;
+}
+
+NTSTATUS
+KmdfEmptyDemoEvtDeviceAdd(
+	_In_    WDFDRIVER       Driver,
+	_Inout_ PWDFDEVICE_INIT DeviceInit
+)
+{
+	// We're not using the driver object,
+	// so we need to mark it as unreferenced
+	UNREFERENCED_PARAMETER(Driver);
+
+	NTSTATUS status;
+
+	// Allocate the device object
+	WDFDEVICE hDevice;
+
+	// Print "Hello World"
+	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"));
+
+	// Create the device object
+	status = WdfDeviceCreate(&DeviceInit,
+		WDF_NO_OBJECT_ATTRIBUTES,
+		&hDevice
+	);
+	return status;
+}
---
+++ Modules/ProfileTest/WinDrivers/KMDFEmptyDemo/KMDFEmptyDemo/ReadMe.txt
@@ -0,0 +1,30 @@
+========================================================================
+    KMDFEmptyDemo Project Overview
+========================================================================
+
+This file contains a summary of what you will find in each of the files that make up your project.
+
+KMDFEmptyDemo.vcxproj
+    This is the main project file for projects generated using an Application Wizard.
+    It contains information about the version of the product that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+KMDFEmptyDemo.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard.
+    It contains information about the association between the files in your project
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+Driver.c
+    Simple create base KMDF driver code.
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+Learn more about Kernel Mode Driver Framework here:
+
+https://docs.microsoft.com/en-us/windows-hardware/drivers/gettingstarted/writing-a-very-small-kmdf--driver
+
+/////////////////////////////////////////////////////////////////////////////
